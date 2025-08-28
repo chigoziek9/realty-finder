4a834957ea2eb02aed5f05988d7c43edcba171d3
Nony <nonybrighto@gmail.com>
2025-08-29 00:28:43 +0100

    nav settings

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
 import { Menu, X, ChevronDown } from "lucide-react";
-import logo from "../assets/logo.png"; // replace with your logo
+import logo from "../assets/NavLogo.png"; // replace with your logo
 
 export default function Navbar() {
   const [isOpen, setIsOpen] = useState(false);
