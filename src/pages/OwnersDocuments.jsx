c762e5413077e4de68494763a681ce726b3d6de2
Nony <nonybrighto@gmail.com>
2025-09-19 15:23:22 +0100

    changes

---
+++ src/pages/OwnersDocuments.jsx
@@ -0,0 +1,117 @@
+import { Clock, Bell, Heart, Settings, LogOut, X, Trash2 } from "lucide-react";
+
+import { useNavigate, useLocation } from "react-router-dom";
+import { AuthContext } from "../AuthContext.jsx";
+import { useContext } from "react";
+import Graph from "../components/Graph.jsx";
+import RecentActivity from "../components/RecentActivity.jsx";
+import InboxInquiry from "../components/InboxInquiry.jsx";
+
+export default function OwnersDocuments() {
+  const navigate = useNavigate();
+  const location = useLocation();
+  const { user } = useContext(AuthContext);
+
+  // Utility to check if a link is active
+  const isActive = (path) =>
+    location.pathname === path
+      ? "bg-white text-green-900 font-medium"
+      : "hover:bg-green-800";
+
+
+  return (
+    <div className="flex min-h-screen bg-gray-100">
+      {/* Sidebar */}
+      <aside className="w-70 bg-green-900 text-white flex flex-col justify-between">
+        <div>
+          {/* Menu */}
+          <nav className="mt-[85px] space-y-1">
+            <button
+              onClick={() => navigate("/owners-dashboard")}
+              className={`flex w-full items-center space-x-3 px-6 py-3 rounded-r-lg  hover:text-green-900 hover:bg-white  ${isActive(
+                "/owners-dashboard"
+              )}`}
+            >
+              <Clock size={18} />
+              <span>Dashboard </span>
+            </button>
+
+            <button
+              onClick={() => navigate("/owners-listings")}
+              className={`flex w-full items-center space-x-3 px-6 py-3 rounded-r-lg  hover:text-green-900 hover:bg-white ${isActive(
+                "/owners-listings"
+              )}`}
+            >
+              <Bell size={18} />
+              <span>My-Listings</span>
+            </button>
+
+            <button
+              onClick={() => navigate("/owners-saved-property")}
+              className={`flex w-full items-center space-x-3 px-6 py-3 rounded-r-lg  hover:text-green-900 hover:bg-white  ${isActive(
+                "/owners-saved-property"
+              )}`}
+            >
+              <Heart size={18} />
+              <span>My saved property</span>
+            </button>
+            <button
+              onClick={() => navigate("/owners-documents")}
+              className={`flex w-full items-center space-x-3 px-6 py-3 rounded-r-lg ${isActive(
+                "/owners-documents"
+              )}`}
+            >
+              <Heart size={18} />
+              <span>My-documents</span>
+            </button>
+            <button
+              onClick={() => navigate("/owners-agreement")}
+              className={`flex w-full items-center space-x-3 px-6 py-3 rounded-r-lg ${isActive(
+                "/owners-agreement"
+              )}`}
+            >
+              <Heart size={18} />
+              <span>New tenancy agreement</span>
+            </button>
+
+            <button
+              onClick={() => navigate("/owners-settings")}
+              className={`flex w-full items-center space-x-3 px-6 py-3 border-t border-green-700 mt-4 rounded-r-lg  hover:text-green-900 hover:bg-white ${isActive(
+                "/owners-settings"
+              )}`}
+            >
+              <Settings size={18} />
+              <span>Account Settings</span>
+            </button>
+          </nav>
+        </div>
+
+        {/* User Info */}
+        <div className="p-6 border-t border-green-800">
+          <div className="flex items-center space-x-3">
+            <img
+              src={user?.profilePic || "https://via.placeholder.com/40"}
+              alt="profile"
+              className="w-10 h-10 rounded-full object-cover border"
+            />
+            <div className="flex-colunm">
+              <p className="font-medium ">
+                {user?.firstName} {user?.lastName}
+              </p>
+              <p className="text-sm text-gray-300">{user?.email}</p>
+            </div>
+          </div>
+          <button className="flex items-center space-x-2 text-red-400 mt-4 hover:text-red-300">
+            <LogOut size={18} />
+            <span>Logout</span>
+          </button>
+        </div>
+      </aside>
+
+      {/* Main Content */}
+      <main className="flex-1 p-[60px]">
+        
+      </main>
+    </div>
+  );
+}
