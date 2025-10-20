0cc8b8d5b0d782170320e177b8141653759227da
Nony <nonybrighto@gmail.com>
2025-10-20 05:50:49 +0100

    feed

--- src/pages/Feed.jsx
+++ src/pages/Feed.jsx
@@ -114,14 +114,14 @@ export default function Feed() {
             Check back shortly for the latest listings.
           </p>
         </div>
-        {/* <DatePicker />
+       <DatePicker />
        <Link
           to="/favorites"
           className="flex items-center text-gray-600 hover:text-gray-900"
         >
           <FaArrowLeft className="w-5 h-5 mr-1" />
           <span className="text-sm">FAvorites</span>
-        </Link>*/}
+        </Link>
       </div>
       <Footer />
     </>
